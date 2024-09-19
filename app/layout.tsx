f3bb07de0c24d88dc684165d95baa79aac451655
Aditya Gupta <aditya1282003@gmail.com>
2024-09-19 23:39:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VLSK264YKL"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-VLSK264YKL');
+        </script>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
